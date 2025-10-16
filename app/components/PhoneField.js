97d11c84cfb9438331335a231d49c8413d4ee3a1
gestiones-art <gestiones@estudiotekton.com>
2025-10-16 17:10:03 -0300

    Update PhoneField.js

--- app/components/PhoneField.js
+++ app/components/PhoneField.js
@@ -1,15 +1,1 @@
-"use client";
 
-export default function PhoneField(props) {
-  return (
-    <input
-      type="tel"
-      inputMode="tel"
-      autoComplete="tel"
-      {...props}
-      onInput={(e) => {
-        e.target.value = e.target.value.replace(/[^0-9+()\-.\s]/g, "");
-      }}
-    />
-  );
-}
