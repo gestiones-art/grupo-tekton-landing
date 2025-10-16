d8e4a73996c023b23284c136a3f0fd7f892434c0
gestiones-art <gestiones@estudiotekton.com>
2025-10-16 18:06:57 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body className="antialiased">{children}</body>
+      <body className="bg-gray-100 text-gray-900">
+        {children}
+      </body>
     </html>
   );
 }
