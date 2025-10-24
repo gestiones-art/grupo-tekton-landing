f5ef7a744a677e6e0b8c7f5339da467ae500a47e
gestiones-art <gestiones@estudiotekton.com>
2025-10-24 08:42:17 -0300

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,10 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://estudiotekton.com',
   generateRobotsTxt: true,
+  exclude: [
+    '/tramites/electromecanica-ascensor', // excluye este trámite del SEO principal
+  ],
+  priority: null,
+  changefreq: 'monthly',
 };
